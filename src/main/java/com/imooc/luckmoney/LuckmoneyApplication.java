84abf24fa356dfd286dd85efa8105f7dcb83c2ee
fta <1018598742@qq.com>
2019-12-24 10:10:56 +0800

    fix:修改为https;http 和 https 同时存在

--- src/main/java/com/imooc/luckmoney/LuckmoneyApplication.java
+++ src/main/java/com/imooc/luckmoney/LuckmoneyApplication.java
@@ -1,7 +1,11 @@
 package com.imooc.luckmoney;
 
+import org.apache.catalina.connector.Connector;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class LuckmoneyApplication {
@@ -10,4 +14,20 @@ public class LuckmoneyApplication {
         SpringApplication.run(LuckmoneyApplication.class, args);
     }
 
+
+    @Bean
+    public ServletWebServerFactory servletContainer() {
+        TomcatServletWebServerFactory tomcat = new TomcatServletWebServerFactory();
+        tomcat.addAdditionalTomcatConnectors(createHTTPConnector());
+        return tomcat;
+    }
+
+    private Connector createHTTPConnector() {
+        Connector connector = new Connector("org.apache.coyote.http11.Http11NioProtocol");
+        connector.setScheme("http");
+        connector.setSecure(false);
+        connector.setPort(8080);
+        connector.setRedirectPort(8443);
+        return connector;
+    }
 }
